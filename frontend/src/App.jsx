3144f6e56c01a7fd6d46572d90fdfeb89aa7d4fb
Abhishek Bhargav <abhishekbhargav8380@gmail.com>
2026-09-14 11:47:25 +0530

    Connect frontend to production backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE = "https://media-sequencer-mq31.onrender.com";
 
 function App() {
 const [windows, setWindows] = useState([]);
